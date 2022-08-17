88dc7fe4de577ed7708a8e5bf1b3c5c536ccb759
Carmen Elisa Grajales Vasquez <cegrajales@sura.com.co>
2022-08-17 17:38:16 -0500

    Se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,17 @@
+# Hola me llamo comentario de línea
+
+'''  Hola soy un comentario de bloque  
+Datos de entrada = variables
+'''
+
+# Datos primitivos 
+# Variables con datos seteados
+
+nombre = "Elisa"
+edad=33
+estatura=1.62
+hinchadelmejor=True
+
+#Salidas
+print(nombre)
+print(f'hola {nombre} tu estatura es {estatura}')
